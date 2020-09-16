4613a056e9479026190231c4bac7bd7169f72e72
Ivan <ivan1501@gmail.com>
2020-09-16 08:23:22 +0200

    Some todos added

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -2,13 +2,36 @@ import React from 'react'
 
 class TodoContainer extends React.Component
 {
+
+    state = {
+        todos: [
+            {
+                id: 1,
+                title: "Create todo app",
+                completed: true
+            },
+            {
+                id: 2,
+                title: "Commit to Github",
+                completed: false
+            },
+            {
+                id: 3,
+                title: "Fix bugs",
+                completed: false
+            }
+        ]
+    };
+
     render()
     {
         return (
             <div>
-                <h1>Test component</h1>
+                {this.state.todos.map(todo => (
+                    <li>{todo.title}</li>
+                ))}
             </div>
-        )
+        );
     }
 }
 
