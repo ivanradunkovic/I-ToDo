1fd7eb88a6886e4aa5baca48527529a6be4f282a
Ivan <ivan1501@gmail.com>
2020-09-16 08:18:32 +0200

    Test component

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class TodoContainer extends React.Component
+{
+    render()
+    {
+        return (
+            <div>
+                <h1>Test component</h1>
+            </div>
+        )
+    }
+}
+
+export default TodoContainer
