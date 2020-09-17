5364c8efac1a672c68a6c795d9555922ca00e15f
Ivan <ivan1501@gmail.com>
2020-09-17 07:58:07 +0200

    Code cleanup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
-class App extends React.Component
-{
-    render()
-    {
-        return (
-            <div>
-                <h1>Todos</h1>
+
+render() {
+    return (
+        <Router>
+            <div className="App">
+                <div className="container">
+                    <Header />
+                    <br />
+                    <Route exact path="/" render={props => (
+                        <React.Fragment>
+                            <AddTodo addTodo={this.addTodo} />
+                            <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
+                        </React.Fragment>
+                    )} />
+
+                    <Route path="/about" component={About} />
+                </div>
             </div>
-        );
-    }
+        </Router>
+    );
+}
 }
 
 export default App;
